a9586d342aabd20d540648845009374665f86b3d
Noella Spitz <noella.spitz@gmail.com>
2024-01-27 23:02:54 +0000

    Add bio

--- src/hero.ts
+++ src/hero.ts
@@ -1,5 +1,6 @@
 const heroImagePath: string = "../public/hero/";
 const heroImages: Array<HTMLImageElement> = [];
+const hero = document.querySelector<HTMLDivElement>("#hero-img")!;
 
 const getImg = (index: number) => {
   return(`${heroImagePath}${index}.jpg`);
@@ -11,11 +12,8 @@ for (let i = 1; i < 115; i++) {
   heroImages.push(img)
 }
 
-const hero = document.querySelector<HTMLDivElement>("#hero-img")!;
-hero.style.backgroundImage = `url(${heroImages[(115 + window.scrollY ) % 114].src})`;
-
 window.addEventListener("scroll", () => {
-  if(heroImages) {
+  if(heroImages[(115 + window.scrollY ) % 114] != undefined) {
     hero.style.backgroundImage = `url(${heroImages[(115 + window.scrollY ) % 114].src})`;
   }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     extend: {
       colors: {
         "background": "#27272a",
-        "foreground": "#1A1818"
+        "foreground": "#1A1818",
+        "foreground-transparent": "rgba(39, 39, 42, 0.2)",
+        "background-darker": "#19191D",
       }
     },
   },
