5c12baffae38ac4b5ef6758c125601e55851eec6
Noella Spitz <noella.spitz@gmail.com>
2024-01-28 21:31:24 +0000

    Fix carousel

--- src/carousel.ts
+++ src/carousel.ts
@@ -1,61 +1,62 @@
-const carouselItems = [
-  `
-  <div id="carousel-0" class="flex flex-col gap-5">
-  <h3 class="text-md tracking-tight font-semibold">FEATURED</h3>
-  <br>
-  <h2 class="text-2xl">The first GCC national to join UK Research and Innovation.</h2>
-  <div class="flex flex-row font-extralight gap-5">
-    <div class="border-b text-center whitespace-nowrap flex flex-row gap-1 text-slate-300 hover:text-white">
-      <img src="./public/icons/external-link.svg" width="12" class="inline">
-      <a target="_blank" rel="noopener noreferrer"
-        href="https://www.muscatdaily.com/2023/10/16/breaking-barriers-omani-teen-first-from-gcc-to-join-ukri/"
-        class="inline">Muscat Daily
-      </a>
-    </div>
-    <div class="border-b text-center whitespace-nowrap flex flex-row gap-1 text-slate-200 hover:text-white">
-      <img src="./public/icons/external-link.svg" width="12" class="inline">
-      <a target="_blank" rel="noopener noreferrer"
-        href="https://www.omanobserver.om/article/1144315/business/economy/young-omani-scientist-is-first-gcc-national-to-be-employed-by-top-uk-research-centre"
-        class="inline">Oman Observer
-      </a>
-    </div>
-  </div>
-  `,
-  `
-  <div id="carousel-1" class="flex flex-col gap-5">
-        <h3 class="text-md tracking-tight font-semibold">UPCOMING BOOK</h3>
-        <br>
-        <div class="px-4">
-          <h2 class="text-3xl font-semibold">Seif Flight
-          </h2>
-          <h3 class="text-2xl font-light">
-            The Story of My Father, Seif Al
-            Mugheiry
-          </h3>
-        </div>
-        <br>
-        <a href="#seif-flight">
-          <img src="./public/icons/down-caret.svg" width="12" class="mx-auto">
-        </a>
-      </div>
-  `,
-]
+const carousel = document.querySelector<HTMLElement>("#carousel")!;
+const carouselItems = document.querySelectorAll<HTMLDivElement>("#carousel > div");
+let carouselIndex = 0;
 
-const carousel: HTMLElement = document.querySelector<HTMLElement>("#carousel")!;
-let carouselIndex = 0
+let transitionEndType = (() => {
+  let el = <HTMLElement>document.createElement('fakeelement');
+  let transitions = {
+    'transition':'transitionend',
+    'OTransition':'oTransitionEnd',
+    'MozTransition':'transitionend',
+    'WebkitTransition':'webkitTransitionEnd'
+  }
 
-const rotateCarousel = async (index: number) => {
-  carousel.innerHTML = carouselItems[index];
+  for(let t in transitions){
+      if( el.style[t] !== undefined ){
+          return transitions[t];
+      }
+  }
+})();
+
+carouselItems.forEach((item: HTMLDivElement) => {
+  item.classList.add("hidden");
+  item.addEventListener(transitionEndType, () => { 
+    hideCarouselItem(item); 
+  }, false);
+});
+
+const showCarouselItem = (element: HTMLElement) => {
+  if(element.classList.contains("fade")) {
+    element.classList.remove("hidden");
+    element.classList.add("invisible");
+    element.classList.add("flex");
+    element.classList.remove("invisible");
+    setTimeout(()=>{
+      element.classList.remove("fade");
+    }, 100)
+  }
+  else
+  {
+    element.classList.add("fade");
+  }
 }
 
-rotateCarousel(0)
-
-let intervalId = setInterval(() => {
-  rotateCarousel(carouselIndex);
-  if (carouselIndex < carouselItems.length - 1) {
-    ++carouselIndex;
+const hideCarouselItem = (element: HTMLElement) => {
+  if(element.classList.contains("fade")) {
+    element.classList.remove("flex");
+    element.classList.add("hidden");
+    if (carouselIndex == carouselItems.length - 1) carouselIndex = 0; 
+    carouselIndex++;
+    showCarouselItem(carouselItems[carouselIndex]);
   }
   else {
-    carouselIndex = 0;
+    setTimeout(() => {
+      showCarouselItem(carouselItems[carouselIndex]);
+    }, 4000);
   }
-}, 4000);
+}
+
+showCarouselItem(carouselItems[carouselIndex]);
+
+
+
